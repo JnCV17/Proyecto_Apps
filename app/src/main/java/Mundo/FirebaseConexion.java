ce3d64ca1b43dc5e423e814587f7319316959b4a
Raichu <sebastian.estrada@uao.edu.co>
2019-05-25 16:06:55 -0500

    Se añadió Clase FirebaseConexion

---
+++ app/src/main/java/Mundo/FirebaseConexion.java
@@ -0,0 +1,86 @@
+package Mundo;
+
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.widget.Toast;
+
+public class FirebaseConexion {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private String userID;
+
+    //vars
+    private Context mContext;
+
+    public FirebaseConexion(Context context) {
+        mAuth = FirebaseAuth.getInstance();
+
+        mContext = context;
+
+
+    }
+
+    public void onStart() {
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            userID = currentUser.getUid();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication Succesful",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+
+    private void escribirBD(){
+
+
+
+    }
+
+
+
+}
